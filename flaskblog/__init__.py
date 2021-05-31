8476d00330b6a82119845cbba070bdb70fc8893f
techsandeep90 <sandeeptech90@gmail.com>
2021-05-31 07:21:42 +0530

    Reset Email And Password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd43e20f1e95f70803b75ac1e0c6a713f'
@@ -11,5 +13,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'gmail-smtp-msa.l.google.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+# app.config['MAIL_USERNAME'] = 'sandeeptech90@gmail.com'
+# app.config['MAIL_PASSWORD'] = 'testing'
+mail = Mail(app)
 
 from flaskblog import routes
