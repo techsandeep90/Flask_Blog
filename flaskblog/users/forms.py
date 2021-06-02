253aac04cf97610b93d5d77004e3651f8bda1b42
techsandeep90 <sandeeptech90@gmail.com>
2021-06-02 06:31:57 +0530

    Blueprints

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flaskblog.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -55,13 +56,6 @@ class UpdateAccountForm(FlaskForm):
           user = User.query.filter_by(email=email.data).first()
           if user:
               raise ValidationError('That email is taken. Please choose a different one.')
-
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
 
 
 class RequestResetForm(FlaskForm):
